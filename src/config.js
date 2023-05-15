171612161b5f1ea75bef557be93d332ff7bff3fb
Slenzy <132808649+Slenzy1840@users.noreply.github.com>
2023-05-15 10:40:37 +0300

    Delete src directory

--- src/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  token: "bot-token",
-  mongo: "mongo-url",
-  davet: "bot-davet-linki",
-  destek: "destek-sunucusu"
-}
--- src/events/ready.js
+++
@@ -1,8 +0,0 @@
-const { ActivityType } = require("discord.js")
-module.exports = {
-	name: 'ready',
-	once: true,
-	execute(client) {
-    let activities = [ `discord.gg/altyapilar`, `${client.user.username}` ], i = 0;
-    setInterval(() => client.user.setActivity({ name: `${activities[i++ % activities.length]}`, type: ActivityType.Listening }), 22000);
-}};
